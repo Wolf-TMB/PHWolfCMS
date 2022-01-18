308d7fe6731c4f36030b4d894987eda74c85c0f2
Wolf_TMB <iluha.tmb@ya.ru>
2022-01-18 17:28:07 +0300

    Добавлена система логирования запросов

--- app/Database.php
+++ app/Database.php
@@ -18,6 +18,7 @@ class Database {
     public function getRecord($sql, $params = null, $orderColumn = 'id', $orderType = 'ASC'): array|bool {
         $sql .= " ORDER BY $orderColumn $orderType LIMIT 1";
         $stmt = $this->connect->prepare($sql);
+        $this->log($stmt->queryString, $params);
         $stmt->execute($params);
         return $stmt->fetch();
     }
@@ -26,33 +27,50 @@ class Database {
         $sql .= " ORDER BY $orderColumn $orderType";
         $sql .= ($limit == 0) ? "" : " LIMIT $limit";
         $stmt = $this->connect->prepare($sql);
+        $this->log($stmt->queryString, $params);
         $stmt->execute($params);
         return $stmt->fetchAll();
     }
 
     public function insert($table, $params): bool {
+        $params = array_merge($params, array(
+            'created_at' => time(),
+            'updated_at' => time()
+        ));
         $columns = array_keys($params);
         $sql = "INSERT INTO $table (".implode(',', $columns).") VALUES(:".implode(', :', $columns).")";
         $stmt = $this->connect->prepare($sql);
-        echo '<pre>';
-            print_r($_SERVER);
-        echo '</pre>';
-        die();
-        die($stmt->queryString);
+        $this->log($stmt->queryString, $params);
         return $stmt->execute($params);
     }
 
     public function update($sql, $params): bool {
+        $params = array_merge($params, array(
+            'updated_at' => time()
+        ));
         $stmt = $this->connect->prepare($sql);
+        $this->log($stmt->queryString, $params);
         return $stmt->execute($params);
     }
 
     public function execute($sql, $params): bool {
         $stmt = $this->connect->prepare($sql);
+        $this->log($stmt->queryString, $params);
         return $stmt->execute($params);
     }
 
     private function log($sql, $params) {
-
+        global $app;
+        if ($app->config->get('DB_SQL_LOG')) {
+            $stmt = $this->connect->prepare('INSERT INTO log_sql (query, params, page, user_id, created_at, updated_at) VALUES (:query, :params, :page, :user_id, :created_at, :updated_at)');
+            $stmt->execute(array(
+                'query' => $sql,
+                'params' => json_encode($params),
+                'page' => $_SERVER['REQUEST_URI'],
+                'user_id' => 0,
+                'created_at' => time(),
+                'updated_at' => time()
+            ));
+        }
     }
 }
