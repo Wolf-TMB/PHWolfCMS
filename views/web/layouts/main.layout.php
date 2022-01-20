94022ca4281fda11da96ceb2ffe2bd2706a5c35d
Wolf_TMB <iluha.tmb@ya.ru>
2022-01-20 14:03:54 +0300

    Добавлен bootstrap, popperjs

--- views/web/layouts/main.layout.php
+++ views/web/layouts/main.layout.php
@@ -6,7 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/">
+    <link rel="stylesheet" href="/resources/css/style.css">
+    <script src="/node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
     {{block:header}}
