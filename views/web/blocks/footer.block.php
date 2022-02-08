c7fc946c02a26f23f05e61fe2f66d493897ada93
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-08 19:14:29 +0300

    Хуинитиал коммит

--- views/web/blocks/footer.block.php
+++ views/web/blocks/footer.block.php
@@ -1,3 +1,13 @@
-<br>
-<br>
-footer
+<div class="footer mt-5 text-center text-white">
+    <div class="d-flex flex-column p-2" style="background: linear-gradient(to right,#252449,#24243e); padding: 0">
+        <span>
+            Копирование информации без разрешения администрации запрещено.
+        </span>
+        <span>
+            Все права на игру принадлежат Mojang AB. Приобрести игру можно на официальном сайте.
+        </span>
+        <span>
+            WilyCraft© 2022 Проект WilyCraft
+        </span>
+    </div>
+</div>
---
+++ views/web/templates/servers.php
@@ -0,0 +1,31 @@
+<div class="col-12 col-lg-8 mt-5">
+    <div class="text-center">
+        <div><img src="/resources/img/servers/skyblock.png" alt=""></div>
+        <div class="fs-2">TechnoMagicSkyBlock</div>
+        <div class="fs-4">1.12.2</div>
+        <p class="fs-5 border-bottom">Этот сервер позволит вам создать и настроить автоматический завод по производству ресурсов и крафту предметов. Сервер подходит для тех, кто любит создавать большие и сложные механизированные системы по добыче ресурсов и созданию предметов.</p>
+        <div class="text-start">
+            <span class="me-4">GROVANASOLO</span><span class="me-4">Intel</span><span class="me-4">Mussed</span>
+        </div>
+    </div>
+    <hr>
+    <div class="text-center">
+        <div><img src="/resources/img/servers/magic.png" alt=""></div>
+        <div class="fs-2">TechnoMagicAdventure</div>
+        <div class="fs-4">1.12.2</div>
+        <p class="fs-5 border-bottom">На этом сервере у вас есть возможность изучить тайны магии, найти в мире загадочные структуры и познать новые возможности. Сервер подходит тем, кто любит изучать новое, внимательно изучать каждую зацепку и копать до сути.</p>
+        <div class="text-start">
+            <span class="me-4">GROVANASOLO</span><span class="me-4">ABI</span><span class="me-4">Tsourai</span>
+        </div>
+    </div>
+    <hr>
+    <div class="text-center">
+        <div><img src="/resources/img/servers/factory1.png" alt=""></div>
+        <div class="fs-2">MaxIndustrial</div>
+        <div class="fs-4">1.12.2</div>
+        <p class="fs-5 border-bottom">Добывать тонны ресурсов и жечь тонны угля - это про данный сервер. Добывай в шахтах огромное количество руды, исследуй новые ископаемые и руды, а так же расширяй свои заводы.</p>
+        <div class="text-start">
+            <span class="me-4">GROVANASOLO</span><span class="me-4">Kn0pka</span><span class="me-4">Jesard</span>
+        </div>
+    </div>
+</div>
