7d2750b643d7a87148aece772c6cc138f2528f90
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-10 17:58:11 +0300

    Добавил страницу голосования

---
+++ views/web/templates/vote.php
@@ -0,0 +1,9 @@
+<div class="col-12 col-lg-8 mt-5">
+    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">MCTOP</a><span class="badge bg-primary">5</span></div>
+    <hr>
+    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">MCRATE</a><span class="badge bg-primary">5</span></div>
+    <hr>
+    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">TOPCRAFT</a><span class="badge bg-primary">5</span></div>
+    <hr>
+    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">MINECRAFTRATING</a><span class="badge bg-primary">5</span></div>
+</div>
