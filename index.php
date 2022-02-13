19c74434d83d9946bdbeaeec78f0df813661b20c
Ilya Levin <iluha.tmb@ya.ru>
2022-02-13 23:31:11 +0300

    Исправлен счётчик времени генерации страницы

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $timeEnd = hrtime(true);
 $executeTime = ($timeEnd - $timeStart) / 1e+6;
 
 $URIData = explode('/', rtrim(ltrim($app->requestURI,'/'), '/'));
-if ($URIData[0] != $app->config->get('ROUTER_API_PREFIX')) {
+if ($URIData[0] != '/api') {
     echo '
         <div style="background-color: black; color: white; position: fixed; top: 0; right: 0; padding: 4px; z-index: 99999;">
             '. $executeTime .' ms
