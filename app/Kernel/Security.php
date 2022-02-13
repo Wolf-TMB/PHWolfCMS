355948246eafa864967876923f8bf27fcbfb6f93
Ilya Levin <iluha.tmb@ya.ru>
2022-02-13 06:18:05 +0300

    Изменён набор данных для генерации CSRF токена

--- app/Kernel/Security.php
+++ app/Kernel/Security.php
@@ -13,10 +13,6 @@ class Security {
             'session_id' => session_id(), //Получаем идентификатор сессии
             'remote_addr' => (@$_SERVER['REMOTE_ADDR']) ?? 'remote_addr', //ip
             'http_x_real_ip' => (@$_SERVER['HTTP_X_REAL_IP']) ?? 'http_x_real_ip', //ip
-            'redirect_mmbr_addr' => (@$_SERVER['REDIRECT_MMDB_ADDR']) ?? 'redirect_mmbr_addr', //ip
-            'redirect_geoio_addr' => (@$_SERVER['REDIRECT_GEOIP_ADDR']) ?? 'redirect_geoio_addr', //ip
-            'redirect_geoip_longitude' => (@$_SERVER['REDIRECT_GEOIP_LONGITUDE']) ?? 'redirect_geoip_longitude', //Долгота по ip
-            'redirect_geoip_latitude' => (@$_SERVER['REDIRECT_GEOIP_LATITUDE']) ?? 'redirect_geoip_latitude', //Широта по ip
             'geoip_country_name' => (@$_SERVER['GEOIP_COUNTRY_NAME']) ?? 'geoip_country_name', //Название страны по ip
             'geoip_country_code' => (@$_SERVER['GEOIP_COUNTRY_CODE']) ?? 'geoip_country_code', //Код страны по ip
             'geoip_region' => (@$_SERVER['GEOIP_REGION']) ?? 'geoip_region', //Регион по ip
