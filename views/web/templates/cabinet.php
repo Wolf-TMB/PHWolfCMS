1a0de010743d731a769d82392a0901ed7958efb8
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-13 17:23:42 +0300

    Небольшие изменения

--- views/web/templates/cabinet.php
+++ views/web/templates/cabinet.php
@@ -1,6 +1,38 @@
 <div class="col-12 col-lg-8 mt-5">
-    <div>WilyFOx</div>
-    <div>WilyFOx</div>
-    <div>WilyFOx</div>
+    <div class="d-flex flex-row">
+        <span  class="my-auto">
+            <img src="/resources/img/favicon/android-chrome-512x512.png" alt="" style="width: 250px; border-radius: 50%">
+        </span>
+        <span class="my-auto fs-1 ms-5">
+            WilyFox
+            <span class="badge bg-warning text-dark">Creator</span>
+            <span class="btn-group">
+                <button style="border: 1px solid black" class="btn btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    SkyBlock 1.12.2
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a href="" class="dropdown-item">SkyBlock 1.12.2</a></li>
+                  <li><a href="" class="dropdown-item">TechnoMagic 1.7.10</a></li>
+                  <li><a href="" class="dropdown-item">MaxIndustrial 1.12.2</a></li>
+                  <li><a href="" class="dropdown-item">SteamPunk 1.12.2</a></li>
+                </ul>
+            </span>
+        </span>
+    </div>
+    <hr>
+    <div class="fs-2 mt-5">
+        <div class="d-flex flex-row justify-content-sm-between">Монтеы: 150<span><a href="" class="btn" style="background: white; border: 1px solid; color: black">Пополнить</a></span></div>
+        <div class="d-flex flex-row justify-content-sm-between">Голоса: 120<span><a href="/vote" class="btn" style="background: white; border: 1px solid; color: black">Голосовать</a></span></div>
+    </div>
+    <hr>
+    <div class="fs-2">
+        <span>Дата регистрации: 11.02.2022</span>
+    </div>
+    <hr>
+    <div class="fs-2 ">
+        <div class="d-flex flex-row justify-content-sm-between mb-4">Статус двухфакторной аутентификации: <span class="badge bg-success">Активна</span></div>
+        <div class="d-flex flex-row justify-content-sm-between">Аккаунт ВК: <span class="badge bg-success">Привязан</span></div>
+    </div>
+
 </div>
 
