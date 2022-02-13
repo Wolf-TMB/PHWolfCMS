531bd8007d14f86566c487c5968456afbdc095d3
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-13 21:00:01 +0300

    Изменено отображение статусов

--- views/web/templates/cabinet.php
+++ views/web/templates/cabinet.php
@@ -5,34 +5,56 @@
         </span>
         <span class="my-auto fs-1 ms-5">
             WilyFox
-            <span class="badge bg-warning text-dark">Creator</span>
-            <span class="btn-group">
-                <button style="border: 1px solid black" class="btn btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    SkyBlock 1.12.2
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a href="" class="dropdown-item">SkyBlock 1.12.2</a></li>
-                  <li><a href="" class="dropdown-item">TechnoMagic 1.7.10</a></li>
-                  <li><a href="" class="dropdown-item">MaxIndustrial 1.12.2</a></li>
-                  <li><a href="" class="dropdown-item">SteamPunk 1.12.2</a></li>
-                </ul>
-            </span>
+<!--            <span class="badge bg-warning text-dark">Creator</span>-->
+
         </span>
     </div>
     <hr>
-    <div class="fs-2 mt-5">
-        <div class="d-flex flex-row justify-content-sm-between">Монтеы: 150<span><a href="" class="btn" style="background: white; border: 1px solid; color: black">Пополнить</a></span></div>
+    <table class="table fs-5">
+        <thead>
+            <tr>
+                <th>Сервер</th>
+                <th>Срок действия</th>
+                <th>Группа</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>SkyBlock 1.12.2</td>
+                <td>до 01.01.2022</td>
+                <td><span class="badge bg-warning">Creator</span></td>
+            </tr>
+            <tr>
+                <td>TechnoMagic 1.7.10</td>
+                <td>до 01.01.2022</td>
+                <td><span class="badge bg-warning">Creator</span></td>
+            </tr>
+            <tr>
+                <td>MaxIndustrial 1.12.2</td>
+                <td>до 01.01.2022</td>
+                <td><span class="badge bg-warning">Creator</span></td>
+            </tr>
+            <tr>
+                <td>SteamPunk 1.12.2</td>
+                <td>до 01.01.2022</td>
+                <td><span class="badge bg-warning">Creator</span></td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="fs-5 mt-5">
+        <div class="d-flex flex-row justify-content-sm-between mb-2">Монтеы: 150<span><a href="" class="btn" style="background: white; border: 1px solid; color: black">Пополнить</a></span></div>
         <div class="d-flex flex-row justify-content-sm-between">Голоса: 120<span><a href="/vote" class="btn" style="background: white; border: 1px solid; color: black">Голосовать</a></span></div>
     </div>
     <hr>
-    <div class="fs-2">
+    <div class="fs-5">
         <span>Дата регистрации: 11.02.2022</span>
     </div>
     <hr>
-    <div class="fs-2 ">
-        <div class="d-flex flex-row justify-content-sm-between mb-4">Статус двухфакторной аутентификации: <span class="badge bg-success">Активна</span></div>
-        <div class="d-flex flex-row justify-content-sm-between">Аккаунт ВК: <span class="badge bg-success">Привязан</span></div>
+    <div class="fs-5">
+        <div class="d-flex flex-row justify-content-sm-between mb-4">Статус двухфакторной аутентификации: <span class="badge bg-success my-auto p-2">Активна</span></div>
+        <div class="d-flex flex-row justify-content-sm-between">Аккаунт ВК: <span class="badge bg-success my-auto p-2">Привязан</span></div>
     </div>
+    <hr>
 
 </div>
 
