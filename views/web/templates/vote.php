d28d16206f4215128d560565934606cd3b8feb65
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-15 22:05:09 +0300

    Изменен дизайн страницы голосования

--- views/web/templates/vote.php
+++ views/web/templates/vote.php
@@ -1,9 +1,21 @@
 <div class="col-12 col-lg-8 mt-5">
-    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">MCTOP</a><span class="badge bg-primary">5</span></div>
+    <div class="fs-1 d-flex flex-row justify-content-between">
+        <span class="my-auto px-2 d-flex flex-row"><span>MCTOP</span><span class="badge bg-primary fs-4 ms-5 my-auto">5</span></span>
+        <span class="my-auto border-start px-2"><img src="resources/img/icons/vote.png" style="width: 64px;" alt=""></span>
+    </div>
     <hr>
-    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">MCRATE</a><span class="badge bg-primary">5</span></div>
+    <div class="fs-1 d-flex flex-row justify-content-between">
+        <span class="my-auto px-2 d-flex flex-row">MCRATE<span class="badge bg-primary fs-4 ms-5 my-auto">5</span></span>
+        <span class="my-auto border-start px-2"><img src="resources/img/icons/vote.png" style="width: 64px;" alt=""></span>
+    </div>
     <hr>
-    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">TOPCRAFT</a><span class="badge bg-primary">5</span></div>
+    <div class="fs-1 d-flex flex-row justify-content-between">
+        <span class="my-auto px-2 d-flex flex-row">TOPCRAFT<span class="badge bg-primary fs-4 ms-5 my-auto">5</span></span>
+        <span class="my-auto border-start px-2"><img src="resources/img/icons/vote.png" style="width: 64px;" alt=""></span>
+    </div>
     <hr>
-    <div class="fs-1"><a href="" class="text-decoration-none text-black me-5">MINECRAFTRATING</a><span class="badge bg-primary">5</span></div>
+    <div class="fs-1 d-flex flex-row justify-content-between overflow-scroll">
+        <span class="my-auto px-2 d-flex flex-row">MINECRAFTRATING<span class="badge bg-primary fs-4 ms-5 my-auto">5</span></span>
+        <span class="my-auto border-start px-2"><img src="resources/img/icons/vote.png" style="width: 64px;" alt=""></span>
+    </div>
 </div>
