53de2e0d816a671ed9aa8da71fbee6b422545d6b
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-15 18:24:26 +0300

    Вынесены настройки безопасности на отдельную страницу

--- views/web/templates/cabinet.php
+++ views/web/templates/cabinet.php
@@ -13,77 +13,46 @@ use PHWolfCMS\App;
             <img src="/resources/img/favicon/android-chrome-512x512.png" alt="" style="width: 250px; border-radius: 50%">
         </span>
         <span class="my-auto fs-1 ms-lg-5">
-            WilyFox
+            WilyFox <span class="ms-5"><a href="/settings"><img src="/resources/img/icons/settings.png" alt="" style="width: 32px"></a></span>
         </span>
     </div>
     <hr>
-    <table class="table fs-5">
-        <thead>
-            <tr>
-                <th>Сервер</th>
-                <th>Срок действия</th>
-                <th>Группа</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>SkyBlock 1.12.2</td>
-                <td>до 01.01.2022</td>
-                <td><span class="badge bg-warning">Creator</span></td>
-            </tr>
-            <tr>
-                <td>TechnoMagic 1.7.10</td>
-                <td>до 01.01.2022</td>
-                <td><span class="badge bg-warning">Creator</span></td>
-            </tr>
-            <tr>
-                <td>MaxIndustrial 1.12.2</td>
-                <td>до 01.01.2022</td>
-                <td><span class="badge bg-warning">Creator</span></td>
-            </tr>
-            <tr>
-                <td>SteamPunk 1.12.2</td>
-                <td>до 01.01.2022</td>
-                <td><span class="badge bg-warning">Creator</span></td>
-            </tr>
-        </tbody>
-    </table>
-    <div class="fs-5 mt-5">
+    <div class="fs-5">
         <div class="d-flex flex-row justify-content-between mb-2">Монтеы: 150<span><a href="" class="btn" style="background: white; border: 1px solid; color: black">Пополнить</a></span></div>
         <div class="d-flex flex-row justify-content-between">Голоса: 120<span><a href="/vote" class="btn" style="background: white; border: 1px solid; color: black">Голосовать</a></span></div>
     </div>
     <hr>
-    <div class="fs-5">
-        <span>Дата регистрации: 11.02.2022</span>
-    </div>
-    <hr>
-    <div class="fs-5">
-        <div class="d-flex flex-row justify-content-between mb-4">Статус двухфакторной аутентификации: <span class="badge bg-success my-auto p-2">Активна</span></div>
-        <div class="d-flex flex-row justify-content-between">Аккаунт ВК: <span class="badge bg-success my-auto p-2">Привязан</span></div>
-    </div>
-    <hr>
-    <div class="fs-5">
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-            <label class="form-check-label" for="flexSwitchCheckDefault">Уведомления о попытке входа в аккаунт</label>
-        </div>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-            <label class="form-check-label" for="flexSwitchCheckDefault">Уведомления о входе в аккаунт</label>
-        </div>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-            <label class="form-check-label" for="flexSwitchCheckDefault">Уведомления о выходе из аккаунта</label>
-        </div>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-            <label class="form-check-label" for="flexSwitchCheckDefault">Уведомления о смене пароля</label>
-        </div>
-        <div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>
-            <label class="form-check-label" for="flexSwitchCheckDefault">Уведомления от администрации проекта</label>
-        </div>
-    </div>
+    <table class="table fs-5">
+        <thead>
+        <tr>
+            <th>Сервер</th>
+            <th>Срок действия</th>
+            <th>Группа</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>SkyBlock 1.12.2</td>
+            <td>до 01.01.2022</td>
+            <td><span class="badge bg-warning">Creator</span></td>
+        </tr>
+        <tr>
+            <td>TechnoMagic 1.7.10</td>
+            <td>до 01.01.2022</td>
+            <td><span class="badge bg-warning">Creator</span></td>
+        </tr>
+        <tr>
+            <td>MaxIndustrial 1.12.2</td>
+            <td>до 01.01.2022</td>
+            <td><span class="badge bg-warning">Creator</span></td>
+        </tr>
+        <tr>
+            <td>SteamPunk 1.12.2</td>
+            <td>до 01.01.2022</td>
+            <td><span class="badge bg-warning">Creator</span></td>
+        </tr>
+        </tbody>
+    </table>
     <hr>
     <div class="ww-border rounded mt-3 p-2 text-center uploadskin-block">
         <div class="mb-3">
@@ -138,27 +107,8 @@ use PHWolfCMS\App;
         </tr>
         </tbody>
     </table>
-    <hr>
     <div class="fs-5">
-        Последние логи аутентификации:
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Действие</th>
-                <th>Время</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Вход в аккаунт</td>
-                    <td>01.01.2022</td>
-                </tr>
-                <tr>
-                    <td>Вход в аккаунт</td>
-                    <td>13.02.2022</td>
-                </tr>
-            </tbody>
-        </table>
+        <span>Дата регистрации: 11.02.2022</span>
     </div>
 </div>
 
