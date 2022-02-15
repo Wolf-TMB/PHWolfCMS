c85620726c3ab39ecdfd2fa7a1fdebef6e92216c
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-15 18:48:41 +0300

    Исправлено отображение на мобильных устройствах

--- views/web/blocks/sidebar/login.block.php
+++ views/web/blocks/sidebar/login.block.php
@@ -8,18 +8,18 @@ use PHWolfCMS\App;
 ?>
 
 <div class="registrationForm mb-5 mt-5 border-start py-4">
-    <div class="text-center">
-        <?php
-        $app->html->form()
-            ->action('login')
-            ->method('POST')
-            ->inputText('lgin', 'login', true, 'Логин')
-            ->inputPassword('pass', 'pass', true, 'Пароль')
-            ->button('Войти', 'btn', 'bt')
-            ->print();
-        ?>
-        <a href="/registration" class="btn mt-3" style="background: white; border: 1px solid; color: black; width: 100%; border-radius: 20px;">Регистрация</a>
-    </div>
+<!--    <div class="text-center">-->
+<!--        --><?php
+//        $app->html->form()
+//            ->action('login')
+//            ->method('POST')
+//            ->inputText('lgin', 'login', true, 'Логин')
+//            ->inputPassword('pass', 'pass', true, 'Пароль')
+//            ->button('Войти', 'btn', 'bt')
+//            ->print();
+//        ?>
+<!--        <a href="/registration" class="btn mt-3" style="background: white; border: 1px solid; color: black; width: 100%; border-radius: 20px;">Регистрация</a>-->
+<!--    </div>-->
     <!--    <form>ффф-->
     <!--        <div class="mb-3">-->
     <!--            <input placeholder="ЛОГИН" type="email" class="form-control" id="login" aria-describedby="emailHelp" style="height: 2rem; border-radius: 15px">-->
@@ -33,21 +33,21 @@ use PHWolfCMS\App;
     <!--        </div>-->
     <!--    </form>-->
 
-    <!--    <div class="d-flex flex-row justify-content-sm-evenly">-->
-    <!--        <span class="my-auto">-->
-    <!--            <img style="width: 96px; border-radius: 50%" src="/resources/img/favicon/android-chrome-512x512.png" alt="">-->
-    <!--        </span>-->
-    <!--        <div class="my-auto d-flex flex-column">-->
-    <!--            <span class="fs-2">WilyFox</span>-->
-    <!--            <span class="d-flex flex-row justify-content-sm-between">150<span>120</span></span>-->
-    <!--        </div>-->
-    <!--    </div>-->
-    <!--    <div class="text-center mt-4">-->
-    <!--        <a href="/cabinet" class="btn btn-primary">Личный кабинет</a>-->
-    <!--    </div>-->
-    <!--    <div class="text-center mt-2">-->
-    <!--        <a href="/logout" class="btn btn-light border border-dark">Выйти</a>-->
-    <!--    </div>-->
+        <div class="d-flex flex-row justify-content-evenly">
+            <span class="my-auto">
+                <img style="width: 96px; border-radius: 50%" src="/resources/img/favicon/android-chrome-512x512.png" alt="">
+            </span>
+            <div class="my-auto d-flex flex-column">
+                <span class="fs-2">WilyFox</span>
+                <span class="d-flex flex-row justify-content-between">150<span>120</span></span>
+            </div>
+        </div>
+        <div class="text-center mt-4">
+            <a href="/cabinet" class="btn btn-primary w-75">Личный кабинет</a>
+        </div>
+        <div class="text-center mt-2">
+            <a href="/logout" class="btn btn-light border border-dark w-75">Выйти</a>
+        </div>
 </div>
 
 <style>
