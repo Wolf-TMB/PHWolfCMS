78501033263a5019191db8e3633492c8d02c90e1
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-20 16:58:43 +0300

    Пофикшено отображение таблицы на мобильных устройствах

--- views/web/templates/vote.php
+++ views/web/templates/vote.php
@@ -14,7 +14,7 @@
         <span class="my-auto border-start px-2"><img src="resources/img/icons/vote.png" style="width: 64px;" alt=""></span>
     </div>
     <hr>
-    <div class="fs-1 d-flex flex-row justify-content-between overflow-scroll">
+    <div class="fs-1 d-flex flex-row justify-content-between overflow-auto">
         <span class="my-auto px-2 d-flex flex-row">MINECRAFTRATING<span class="badge bg-primary fs-4 ms-5 my-auto">5</span></span>
         <span class="my-auto border-start px-2"><img src="resources/img/icons/vote.png" style="width: 64px;" alt=""></span>
     </div>
