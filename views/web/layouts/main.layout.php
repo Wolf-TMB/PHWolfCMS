92ef4f651343ec48c43812cfd0dce5c14c38c146
Wolf_TMB <iluha.tmb@ya.ru>
2022-02-25 10:55:26 +0300

    Добавлена библиотека jQuery

--- views/web/layouts/main.layout.php
+++ views/web/layouts/main.layout.php
@@ -23,6 +23,7 @@
 
     <script src="/node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
 </head>
 <body>
     <div class="main flex-column d-flex">
