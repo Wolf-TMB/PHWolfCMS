2d6cede5098289c04c52bc9b59445f9659df2d1f
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-02-26 18:36:09 +0300

    Добавлена проверка аутентификации

--- views/web/blocks/sidebar/login.block.php
+++ views/web/blocks/sidebar/login.block.php
@@ -3,35 +3,37 @@
  * @var App $app
  */
 
-use PHWolfCMS\App;
-
+use PHWolfCMS\Kernel\Modules\App\App;
+$test = \PHWolfCMS\Kernel\Modules\Facade\Auth::attempt('Wolf_TMB', 'WolfTMB123');
+var_dump($test);
 ?>
 
 <div class="registrationForm mb-5 mt-5 border-start py-4">
     <div class="text-center w-100">
-        <?php
-        $app->html->form()
-            ->action('login')
-            ->method('POST')
-            ->inputText('lgin', 'login', true, 'Логин')
-            ->inputPassword('pass', 'pass', true, 'Пароль')
-            ->button('Войти', 'btn w-100 text-white rounded-pill wc-background-gradient', 'bt')
-            ->addElement($app->html->link()->content('Зарегистрироваться')->href('/registration')->addClass('btn w-100 text-black rounded-pill bg-white mt-2 border border-dark')->getHtml(), false)
-            ->print();
-        ?>
-<!--        <div class="d-flex flex-row justify-content-evenly">-->
-<!--            <span class="my-auto">-->
-<!--                <img width="96" class="rounded-circle" src="/resources/img/favicon/android-chrome-512x512.png" alt="">-->
-<!--            </span>-->
-<!--            <div class="my-auto d-flex flex-column">-->
-<!--                <span class="fs-2">WilyFox</span>-->
-<!--                <span class="d-flex flex-row justify-content-between">150<span>120</span></span>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="text-center mt-4">-->
-<!--            <a href="/cabinet" class="btn btn-primary w-75">Личный кабинет</a>-->
-<!--        </div>-->
-<!--        <div class="text-center mt-2">-->
-<!--            <a href="/logout" class="btn btn-light border border-dark w-75">Выйти</a>-->
-        </div>
+        <?php if (!\PHWolfCMS\Kernel\Modules\Facade\Auth::check()): ?>
+            <?php $app->html->form()
+                ->action('login')
+                ->method('POST')
+                ->inputText('lgin', 'login', true, 'Логин')
+                ->inputPassword('pass', 'pass', true, 'Пароль')
+                ->button('Войти', 'btn w-100 text-white rounded-pill wc-background-gradient', 'bt')
+                ->addElement($app->html->link()->content('Зарегистрироваться')->href('/registration')->addClass('btn w-100 text-black rounded-pill bg-white mt-2 border border-dark')->getHtml(), false)
+                ->print(); ?>
+        <?php else: ?>
+            <div class="d-flex flex-row justify-content-evenly">
+            <span class="my-auto">
+                <img width="96" class="rounded-circle" src="/resources/img/favicon/android-chrome-512x512.png" alt="">
+            </span>
+                <div class="my-auto d-flex flex-column">
+                    <span class="fs-2">WilyFox</span>
+                    <span class="d-flex flex-row justify-content-between">150<span>120</span></span>
+                </div>
+            </div>
+            <div class="text-center mt-4">
+                <a href="/cabinet" class="btn btn-primary w-75">Личный кабинет</a>
+            </div>
+            <div class="text-center mt-2">
+                <a href="/logout" class="btn btn-light border border-dark w-75">Выйти</a>
+            </div>
+        <?php endif; ?>
 </div>
