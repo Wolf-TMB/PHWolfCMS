d1efc85f0ed410a9b0154697081291cebd8d1711
Wolf_TMB <iluha.tmb@ya.ru>
2022-03-02 15:02:44 +0300

    Добавлен вывод сообщений об ошибках, автозаполнение полей после ошибочного ввода

--- views/web/templates/registration.php
+++ views/web/templates/registration.php
@@ -3,21 +3,30 @@
  * @var App $app
  */
 
-use PHWolfCMS\App;
+use PHWolfCMS\Kernel\Modules\App\App;
 
+$data = json_decode($app->session->getFlash('registrationError'));
 ?>
 
 <div class="col-12 col-lg-8 mt-5">
     <div class="col-12">
         <div class="col-6 offset-3">
+            <?php if (!empty($data->messages)): ?>
+                <div class="alert alert-danger">
+                        <?php foreach ($data->messages as $message): ?>
+                            - <?= $message ?> <br>
+                        <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
             <?php
                 $app->html->form()
                     ->action('registration')
                     ->method('POST')
-                    ->inputText('login', 'login', true, 'Логин', [])
-                    ->inputText('email', 'email', true, 'E-Mail', [])
-                    ->inputPassword('pass', 'pass', true, 'Пароль', [])
-                    ->inputPassword('pass2', 'pass2', true, 'Повторите пароль', [])
+                    ->csrf_token()
+                    ->inputText('login', 'login', true, 'Логин', ['value' => ($data->data->login) ?? ''])
+                    ->inputText('email', 'email', true, 'E-Mail', ['type' => 'email', 'value' => ($data->data->email) ?? ''])
+                    ->inputPassword('password', 'password', true, 'Пароль')
+                    ->inputPassword('password_confirm', 'password_confirm', true, 'Повторите пароль')
                     ->button('Регистрация', 'btn bg-primary text-white mx-auto')
                     ->print();
             ?>
