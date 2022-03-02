0dfe646e6c246a184f21bd47022c9129e949b20f
Wolf_TMB <iluha.tmb@ya.ru>
2022-03-02 15:01:53 +0300

    Добавлены валидаторы "login", "email"

--- config/validator.config.php
+++ config/validator.config.php
@@ -2,12 +2,24 @@
 return array(
     'validators' => (object) array(
         'password' => (object) array(
-            'class' => \PHWolfCMS\Kernel\Modules\Validator\PasswordValidator::class,
             'validators' => (object) array(
                 'min_length' => 8,
-                'max_length' => 100
+                'max_length' => 255,
+                'regexp' => '/(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]+/'
             )
-        )
+        ),
+        'login' => (object) array(
+            'validators' => (object) array(
+                'min_length' => 3,
+                'max_length' => 30,
+                'regexp' => '/^(?=.*[a-zA-Z])[a-zA-Z0-9_]+$/'
+            )
+        ),
+        'email' => (object) array(
+            'validators' => (object) array(
+                'email' => true
+            )
+        ),
     ),
 
 );
