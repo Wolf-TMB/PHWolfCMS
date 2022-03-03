4ff1ca91ec717ffaa92c0a0fbf487d786edcb31f
Wolf_TMB <iluha.tmb@ya.ru>
2022-03-03 16:32:05 +0300

    Добавлены файловые репозитории для скинов и плащей пользователей

--- config/file_repository.config.php
+++ config/file_repository.config.php
@@ -2,14 +2,24 @@
 
 return array(
     'repositories' => (object) array(
-        'test' => (object) array(
-            'class' => \PHWolfCMS\Kernel\Modules\FileRepository\TestFileRepository::class,
+        'skin' => (object) array(
+            'class' => \PHWolfCMS\Kernel\Modules\FileRepository\SkinFileRepository::class,
             'mime_types' => array(
-                'text/html',
-                'text/plain'
+                'image/jpeg',
+                'image/png'
             ),
-            'max_file_size' => 1000 # байт
-        )
+            'max_file_size' => 4096, # байт
+            'require_auth' => true
+        ),
+        'cloak' => (object) array(
+            'class' => \PHWolfCMS\Kernel\Modules\FileRepository\CloakFileRepository::class,
+            'mime_types' => array(
+                'image/jpeg',
+                'image/png'
+            ),
+            'max_file_size' => 4096, # байт
+            'require_auth' => true
+        ),
     ),
 
     'repo_dir' => 'storage/fileRepositories/{REPO_NAME}/', # {REPO_NAME} - будет заменено на имя репозитория
