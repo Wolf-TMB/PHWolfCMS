8239fc099754ed962fafdef7f6aae35c9eca127b
WilyFoxM <wilyfoxgonewild@gmail.com>
2022-04-07 22:27:00 +0300

    Изменена верстка блоков описания сервера

--- views/web/templates/skyblock.php
+++ views/web/templates/skyblock.php
@@ -9,35 +9,45 @@
     </div>
 
     <div class="mt-5 fs-2">
-        <div class="d-flex flex-row justify-content-between border-bottom"><span>Описание</span><span>Моды</span></div>
+        <div class="d-flex flex-row justify-content-between border-bottom"><span id="descButton">Описание</span><span id="persButton">Персонал</span><span id="modsButton">Моды</span></div>
         <div class="warp">
-            <div id="Desc" class="fs-5 mt-4">
-                Этот сервер позволит вам создать и настроить автоматический завод по производству ресурсов и крафту предметов. Сервер подходит для тех, кто любит создавать большие и сложные механизированные системы по добыче ресурсов и созданию предметов.
+            <div id="Desc" class="fs-4 mt-4">
+                <p>Этот сервер позволит вам создать и настроить автоматический завод по производству ресурсов и крафту предметов. Сервер подходит для тех, кто любит создавать большие и сложные механизированные системы по добыче ресурсов и созданию предметов.</p>
+                <p>Пройдите цепочку квестов, выполняйте интересные задания которые проведут от булыжника по основным модам! Развивайтесь в одиночку или позовите друзей.</p>
             </div>
-            <div id="Mods" class="fs-5 mt-4">
-                Client:
-                -- -DamageIndicators
-                -- -StatusEffectHUD
-                -- -MouseTweaks
-                - - - -NotEnoughItems
-                - - - -NEIAddons
-                - - - -NotEnoughEnergistics
-                - - - -NotEnoughResources
-                - - - -ThaumcraftNIEPlugin
-                -- -OptiFine
-                -- -SmoothFont
-                -- -Waila
-                -- -WailaHarvestability
-                -- -WAILAPlugins
-                -- -Wawla
-                -- -WorldEditCuiFe
+            <div id="Pers" class="fs-4 mt-4 d-none">
 
-                Ex Nihilo
-                --Ex Astris
-                --Ex Compressum
-                Thaumcraft 4
-                --Thaumic Tinkerer
-                ...
+                <table class="table">
+                    <thead class="bg-dark text-white">
+                        <tr>
+                            <th>Роль</th>
+                            <th>Ник</th>
+                        </tr>
+                    </thead>
+                    <tr><td class="me-2 text-danger">Судо-Куратор</td><td>GROVANASOLO</td></tr>
+                    <tr><td class="mb-2 text-danger">Куратор</td><td>Tsourai</td></tr>
+                    <tr><td class="mb-2 text-primary">Гл.Модератор</td><td>Mussed</td></tr>
+                    <tr><td class="mb-2 text-primary">Модератор</td><td>Ravec</td></tr>
+                    <tr><td class="mb-2 text-success">Хелпер</td><td>AMD</td></tr>
+                </table>
+            </div>
+            <div id="Mods" class="fs-5 mt-4 d-none">
+                <div class="mb-2"><span class="fs-2">Клиент:</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/197/432/64/64/636892369205002880.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">DamageIndicators</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/12/89/64/64/635515249256254606.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">StatusEffectHUD</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/467/691/64/64/637749969243449870.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">MouseTweaks</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/8/473/64/64/635404785886717614.jpeg" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">NotEnoughItems</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/5/823/64/64/635351434418961385.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">NEIAddons</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/12/367/64/64/635525842768853257.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">NotEnoughResources</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/11/611/64/64/635496774814911579.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">ThaumcraftNIEPlugin</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/419/301/64/64/637646189487004112.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">NotEnoughEnergistics</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/259/725/64/64/637212229382044594.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">OptiFine</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/138/174/64/64/636519903439155200.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">SmoothFont</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/17/140/64/64/635658524720529782.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">Waila</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/6/741/64/64/635367541803914396.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">WailaHarvestability</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/12/713/64/64/635539701519639153.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">WAILAPlugins</span></div>
+                <div class="mb-4"><span class="ms-6 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/11/204/64/64/635483047149673159.png" alt="" class="wc-wh-32px rounded-circle"></span><span class="fs-5">Wawla</span></div>
+                <div class="mb-4"><span class="ms-4 me-2"><img src="https://media.forgecdn.net/avatars/thumbnails/75/514/64/64/636166153154339907.png" alt="" class="wc-wh-64px rounded-circle"></span><span class="fs-4">WorldEditCui</span></div>
             </div>
         </div>
     </div>
@@ -45,7 +55,32 @@
 </div>
 
 <script>
-    let block = document.getElementById('Desc');
-    let state = 'Desc'
+    let descButton = $('#descButton');
+    let modsButton = $('#modsButton');
+    let persButton = $('#persButton');
+    let desc = document.getElementById('Desc');
+    let mods = document.getElementById('Mods');
+    let pers = document.getElementById('Pers');
+
+    modsButton.on('click', e => {
+        mods.classList.remove('d-none');
+        mods.classList.add('d-block');
+        pers.classList.add('d-none');
+        desc.classList.add('d-none');
+    })
+
+    descButton.on('click', e => {
+        mods.classList.add('d-none');
+        pers.classList.add('d-none');
+        desc.classList.remove('d-none');
+    })
+
+    persButton.on('click', e => {
+        mods.classList.add('d-none');
+        desc.classList.add('d-none');
+        pers.classList.remove('d-none');
+    })
+
+
 
 </script>
